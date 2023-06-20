e1b6d4d0029bf770a8afc3646d4e4b8dda74ed64
Thành Trung <118869650+Trung78z@users.noreply.github.com>
2023-06-20 21:45:40 +0700

    addfilecommit

---
+++ main/webthinkspeak.c
@@ -0,0 +1,293 @@
+/* HTTP GET Example using plain POSIX sockets
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "protocol_examples_common.h"
+
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include "lwip/netdb.h"
+#include "lwip/dns.h"
+#include "sdkconfig.h"
+
+/* Constants that aren't configurable in menuconfig */
+#define WEB_SERVER "api.thingspeak.com"
+#define WEB_PORT "80"
+#define WEB_PATH "/"
+#include <dht11.h>
+#define PIN_IN GPIO_NUM_4
+
+static const char *TAG = "example";
+char REQUEST[512];
+#ifndef DHT11_H_
+#define DHT11_H_
+
+#include "driver/gpio.h"
+
+enum dht11_status
+{
+    DHT11_CRC_ERROR = -2,
+    DHT11_TIMEOUT_ERROR,
+    DHT11_OK
+};
+
+struct dht11_reading
+{
+    int status;
+    int temperature;
+    int humidity;
+};
+
+void DHT11_init(gpio_num_t);
+
+struct dht11_reading DHT11_read();
+
+#endif
+
+#include "esp_timer.h"
+#include "driver/gpio.h"
+#include "rom/ets_sys.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "dht11.h"
+
+static gpio_num_t dht_gpio;
+static int64_t last_read_time = -2000000;
+static struct dht11_reading last_read;
+
+static int _waitOrTimeout(uint16_t microSeconds, int level)
+{
+    int micros_ticks = 0;
+    while (gpio_get_level(dht_gpio) == level)
+    {
+        if (micros_ticks++ > microSeconds)
+            return DHT11_TIMEOUT_ERROR;
+        ets_delay_us(1);
+    }
+    return micros_ticks;
+}
+
+static int _checkCRC(uint8_t data[])
+{
+    if (data[4] == (data[0] + data[1] + data[2] + data[3]))
+        return DHT11_OK;
+    else
+        return DHT11_CRC_ERROR;
+}
+
+static void _sendStartSignal()
+{
+    gpio_set_direction(dht_gpio, GPIO_MODE_OUTPUT);
+    gpio_set_level(dht_gpio, 0);
+    ets_delay_us(20 * 1000);
+    gpio_set_level(dht_gpio, 1);
+    ets_delay_us(40);
+    gpio_set_direction(dht_gpio, GPIO_MODE_INPUT);
+}
+
+static int _checkResponse()
+{
+    /* Wait for next step ~80us*/
+    if (_waitOrTimeout(80, 0) == DHT11_TIMEOUT_ERROR)
+        return DHT11_TIMEOUT_ERROR;
+
+    /* Wait for next step ~80us*/
+    if (_waitOrTimeout(80, 1) == DHT11_TIMEOUT_ERROR)
+        return DHT11_TIMEOUT_ERROR;
+
+    return DHT11_OK;
+}
+
+static struct dht11_reading _timeoutError()
+{
+    struct dht11_reading timeoutError = {DHT11_TIMEOUT_ERROR, -1, -1};
+    return timeoutError;
+}
+
+static struct dht11_reading _crcError()
+{
+    struct dht11_reading crcError = {DHT11_CRC_ERROR, -1, -1};
+    return crcError;
+}
+
+void DHT11_init(gpio_num_t gpio_num)
+{
+    /* Wait 1 seconds to make the device pass its initial unstable status */
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    dht_gpio = gpio_num;
+}
+
+struct dht11_reading DHT11_read()
+{
+    /* Tried to sense too son since last read (dht11 needs ~2 seconds to make a new read) */
+    if (esp_timer_get_time() - 2000000 < last_read_time)
+    {
+        return last_read;
+    }
+
+    last_read_time = esp_timer_get_time();
+
+    uint8_t data[5] = {0, 0, 0, 0, 0};
+
+    _sendStartSignal();
+
+    if (_checkResponse() == DHT11_TIMEOUT_ERROR)
+        return last_read = _timeoutError();
+
+    /* Read response */
+    for (int i = 0; i < 40; i++)
+    {
+        /* Initial data */
+        if (_waitOrTimeout(50, 0) == DHT11_TIMEOUT_ERROR)
+            return last_read = _timeoutError();
+
+        if (_waitOrTimeout(70, 1) > 28)
+        {
+            /* Bit received was a 1 */
+            data[i / 8] |= (1 << (7 - (i % 8)));
+        }
+    }
+
+    if (_checkCRC(data) != DHT11_CRC_ERROR)
+    {
+        last_read.status = DHT11_OK;
+        last_read.temperature = data[2];
+        last_read.humidity = data[0];
+        return last_read;
+    }
+    else
+    {
+        return last_read = _crcError();
+    }
+}
+
+static void http_get_task(void *pvParameters)
+{
+
+    const struct addrinfo hints = {
+        .ai_family = AF_INET,
+        .ai_socktype = SOCK_STREAM,
+    };
+    struct addrinfo *res;
+    struct in_addr *addr;
+    int s, r;
+    char recv_buf[64];
+
+    while (1)
+    {
+        esp_rom_gpio_pad_select_gpio(PIN_IN);
+        gpio_set_direction(PIN_IN, GPIO_MODE_INPUT);
+        DHT11_init(PIN_IN);
+        struct dht11_reading reading = DHT11_read();
+        int a = reading.temperature;
+        int b = reading.humidity;
+        int err = getaddrinfo(WEB_SERVER, WEB_PORT, &hints, &res);
+
+        if (err != 0 || res == NULL)
+        {
+            ESP_LOGE(TAG, "DNS lookup failed err=%d res=%p", err, res);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            continue;
+        }
+
+        /* Code to print the resolved IP.
+
+           Note: inet_ntoa is non-reentrant, look at ipaddr_ntoa_r for "real" code */
+        addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
+        ESP_LOGI(TAG, "DNS lookup succeeded. IP=%s", inet_ntoa(*addr));
+
+        s = socket(res->ai_family, res->ai_socktype, 0);
+        if (s < 0)
+        {
+            ESP_LOGE(TAG, "... Failed to allocate socket.");
+            freeaddrinfo(res);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... allocated socket");
+
+        if (connect(s, res->ai_addr, res->ai_addrlen) != 0)
+        {
+            ESP_LOGE(TAG, "... socket connect failed errno=%d", errno);
+            close(s);
+            freeaddrinfo(res);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+
+        ESP_LOGI(TAG, "... connected");
+        sprintf(REQUEST, "GET https://api.thingspeak.com/update?api_key=QPOS5APHMPJNA24L&field1=%d&field2=%d\n\n", a, b);
+        freeaddrinfo(res);
+
+        if (write(s, REQUEST, strlen(REQUEST)) < 0)
+        {
+            ESP_LOGE(TAG, "... socket send failed");
+            close(s);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... socket send success");
+
+        struct timeval receiving_timeout;
+        receiving_timeout.tv_sec = 5;
+        receiving_timeout.tv_usec = 0;
+        if (setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &receiving_timeout,
+                       sizeof(receiving_timeout)) < 0)
+        {
+            ESP_LOGE(TAG, "... failed to set socket receiving timeout");
+            close(s);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... set socket receiving timeout success");
+
+        /* Read HTTP response */
+        do
+        {
+            bzero(recv_buf, sizeof(recv_buf));
+            r = read(s, recv_buf, sizeof(recv_buf) - 1);
+            for (int i = 0; i < r; i++)
+            {
+                putchar(recv_buf[i]);
+            }
+        } while (r > 0);
+
+        ESP_LOGI(TAG, "... done reading from socket. Last read return=%d errno=%d.", r, errno);
+        close(s);
+        for (int countdown = 10; countdown >= 0; countdown--)
+        {
+            ESP_LOGI(TAG, "%d... ", countdown);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
+        ESP_LOGI(TAG, "Starting again!");
+    }
+}
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
+    xTaskCreate(&http_get_task, "http_get_task", 4096, NULL, 5, NULL);
+}
